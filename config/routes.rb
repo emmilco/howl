64ed033d7174af1843ea4a91cdd1d5cb008c5051
EMMilco <elliot.milco@gmail.com>
2018-03-05 17:41:10 -0500

    Add StaticPagesController, namespace User and Session routes under /api

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  root to: 'static_pages#root'
+
+  namespace :api do
+    resources :users, only: [:create, :show, :update, :destroy, :follow, :unfollow]
+    resource :session, only: [:create, :destroy]
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
