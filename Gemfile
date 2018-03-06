e402de8dd90834dfe3454246e4ad1b7a01c2c237
EMMilco <elliot.milco@gmail.com>
2018-03-05 22:11:58 -0500

    Integrate Unsplash API

--- Gemfile
+++ Gemfile
@@ -32,6 +32,8 @@ gem 'bcrypt', '~> 3.1.7'
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
 
+gem 'unsplash'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- config/application.rb
+++ config/application.rb
@@ -1,5 +1,5 @@
 require_relative 'boot'
-
+require 'unsplash'
 require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
@@ -11,6 +11,12 @@ module Howl
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
+    Unsplash.configure do |config|
+      config.application_id     = "1b15d71a05bb0643510148d8ac9aec20e2b18124e7b72be1375a9f570550bfa2"
+      config.application_secret = "dfcabfafc344bf52da2269098dc6e7dd015f805a9546145c5717fe8499edd52e"
+      config.application_redirect_uri = "https://your-application.com/oauth/callback"
+      config.utm_source = "howl"
+    end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
