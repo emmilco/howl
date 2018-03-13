d0756edae5b8e672f53b374859b3dccef07921e7
EMMilco <elliot.milco@gmail.com>
2018-03-13 15:27:21 -0400

    Style top articles on front page

--- frontend/components/homepage_top_articles.jsx
+++ frontend/components/homepage_top_articles.jsx
@@ -5,37 +5,39 @@ import HomepageFeedItem from './homepage_feed_item';
 const HomepageTopArticles = (props) => {
   if (props.articles.length === 0) {return null;}
   return (
-    <div id="homepageTopArticles">
+    <div id="homepage_top_articles">
       <div className="top_1">
         <HomepageFeedItem
           user={props.users[0]}
           article={props.articles[0]}/>
       </div>
+      <div className="top_2_container">
+        <div className="top_2">
+          <HomepageFeedItem
+            user={props.users[1]}
+            article={props.articles[1]}/>
+        </div>
 
-      <div className="top_2">
-        <HomepageFeedItem
-          user={props.users[1]}
-          article={props.articles[1]}/>
+        <div className="top_2">
+          <HomepageFeedItem
+            user={props.users[2]}
+            article={props.articles[2]}/>
+        </div>
       </div>
 
-      <div className="top_2">
-        <HomepageFeedItem
-          user={props.users[2]}
-          article={props.articles[2]}/>
+      <div className="top_2_container">
+        <div className="top_2">
+          <HomepageFeedItem
+            user={props.users[3]}
+            article={props.articles[3]}/>
+            </div>
+
+        <div className="top_2">
+          <HomepageFeedItem
+            user={props.users[4]}
+            article={props.articles[4]}/>
+        </div>
       </div>
-
-      <div className="top_2">
-        <HomepageFeedItem
-          user={props.users[3]}
-          article={props.articles[3]}/>
-          </div>
-
-      <div className="top_2">
-        <HomepageFeedItem
-          user={props.users[4]}
-          article={props.articles[4]}/>
-      </div>
-
     </div>
   );
 };
